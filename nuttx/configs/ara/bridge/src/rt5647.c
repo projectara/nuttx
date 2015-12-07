7f0ae6b51bafef0988725c490c1c69df949a4156
ChrisMyerchin <myerchin_chris@projectara.com>
2015-12-07 11:37:09 -0800

    subsystem: re-apply bit invert on amp-control

--- nuttx/configs/ara/bridge/src/rt5647.c
+++ nuttx/configs/ara/bridge/src/rt5647.c
@@ -465,7 +465,7 @@ struct audio_control rt5647_spo_r_mix[] = {
 #ifdef FOR_AUDIO_DEMO
 struct audio_control rt5647_spk_amp_switch[] = {
     AUDCTL_BIT("Switch", RT5647_CTL_SPKAMP_SWITCH, MIXER,
-               RT5647_SPKOUT_VOL, RT5647_VOL_L_SFT, 0),
+               RT5647_SPKOUT_VOL, RT5647_VOL_L_SFT, 1),
 };
 #endif
 
