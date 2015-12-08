10c5c4113e4c4d5df067c8533c8026ec523b3d8c
Jean Pihet <jean.pihet@newoldbits.com>
2015-12-08 18:32:19 +0100

    Ara: SVC: DB3: Use ES3 silicon revision of the Switch
    !! Hack warning !!
    The value is hardcoded in the board file.
    Should be runtime detected using the HW_ID feature.
    
    Testing Done: build
    Bug Number: SW-1804
    Review URL:
    Reviewed by:
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- nuttx/configs/ara/svc/src/board-db3.c
+++ nuttx/configs/ara/svc/src/board-db3.c
@@ -399,7 +399,7 @@ static struct ara_board_info db3_board_info = {
         .gpio_irq         = (GPIO_INPUT | GPIO_FLOAT | GPIO_EXTI | \
                              GPIO_PORTC | GPIO_PIN9),
         .irq_rising_edge  = true,
-        .rev              = SWITCH_REV_ES2,
+        .rev              = SWITCH_REV_ES3,
         .bus              = SW_SPI_PORT_2,
         .spi_cs           = (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_OUTPUT_SET | \
                              GPIO_PORTB | GPIO_PIN12)
