afd69f30c1247012ea54ea8b1773152faab9508d
Jean Pihet <jean.pihet@newoldbits.com>
2015-12-16 14:43:59 -0500

    Ara: SVC: power app: Add commands to control the WAKEOUT pulse duration
    The 'power w' command takes an extra optional parameter to specify the
    WAKEOUT pulse duration:
     power wakeout <interface> [<length>]:
        <interface>: Interface to send WAKEOUT to.
        <length>: Pulse length in us.
    
    A new 'power l' command has been added to set the default WAKEOUT pulse
    duration for future WAKEOUTs. A negative value is ignored and so the
    hardcoded values of WAKEOUT duration are used:
     power wakeout_length [<length>]:
        <length>: Pulse duration in us. -1 to use the default hardcoded value
    
    Testing Done: Boot ES3 DB3; control the pulse duration on oscilloscope
    Bug Number: SW-1804
    Review URL:
    Reviewed by:
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- nuttx/configs/ara/svc/src/interface.h
+++ nuttx/configs/ara/svc/src/interface.h
@@ -156,7 +156,7 @@ const char *interface_get_name(struct interface *iface);
 int interface_pwr_enable(struct interface*);
 int interface_pwr_disable(struct interface*);
 enum ara_iface_pwr_state interface_get_pwr_state(struct interface *iface);
-int interface_generate_wakeout(struct interface *, bool assert);
+int interface_generate_wakeout(struct interface *, bool assert, int length);
 int interface_store_hotplug_state(uint8_t port_id, enum hotplug_state hotplug);
 enum hotplug_state interface_consume_hotplug_state(uint8_t port_id);
 enum hotplug_state interface_get_hotplug_state(struct interface *iface);
