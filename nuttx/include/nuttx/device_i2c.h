4cb1814dfc652219e9318ae8c69770d96a7ded72
Joel Porquet <joel@porquet.org>
2015-12-30 10:12:44 +0100

    i2c: introduce new i2c device driver structure
    We have a nice device driver model for almost all the device types (uart, spi,
    hid, etc.) except for i2 which API stayed the NuttX original one.
    
    For the sake of simplicity and coherency across all device types, let's adapt
    i2c to the common device driver model.
    
    This patch introduces a new i2c device structure, based on the original i2c
    API but shrunk down to the minimum of what Greybus needs.
    
    Testing Done: none
    Bug Number: SW-2145
    Review URL: https://reviews.leaflabs.com/r/2727/
    Reviewed by: fabien parent, greg kh, mark greer
    
    Signed-off-by: Joel Porquet <joel@porquet.org>

---
+++ nuttx/include/nuttx/device_i2c.h
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2016 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Joel Porquet
+ */
+
+#ifndef __INCLUDE_NUTTX_DEVICE_I2C_H
+#define __INCLUDE_NUTTX_DEVICE_I2C_H
+
+#include <errno.h>
+#include <nuttx/device.h>
+
+#define DEVICE_TYPE_I2C_HW  "i2c"
+
+/** @defgroup I2C_FLAGS I2C flags
+ * @{
+ */
+#define I2C_FLAG_READ       0x0001 /** Read data, from slave to master */
+#define I2C_FLAG_TEN        0x0002 /** Ten bit addressing */
+#define I2C_FLAG_NORESTART  0x0080 /** Do not start transfer with (re-)start */
+/** @} */
+
+/** I2C request structure */
+struct device_i2c_request {
+    uint16_t addr;      /**< Slave address */
+    uint16_t flags;     /**< Flags (@see I2C_FLAGS) */
+    uint8_t *buffer;    /**< Buffer for data to transfer (read or write) */
+    int length;         /**< Size of the buffer */
+};
+
+/** I2C device driver operations */
+struct device_i2c_type_ops {
+    /** Perform I2C transfers */
+    int (*transfer)(struct device *dev, struct device_i2c_request *requests,
+            uint32_t count);
+};
+
+/**
+ * @brief Perform I2C transfers
+ *
+ * @param dev Pointer to structure of device data
+ * @param requests List of I2C requests (@see struct device_i2c_request)
+ * @param count Number of I2C requests
+ * @return 0 on success, negative errno on error
+ */
+static inline int device_i2c_transfer(struct device *dev, struct
+        device_i2c_request *requests, uint32_t count)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+    if (DEVICE_DRIVER_GET_OPS(dev, i2c)->transfer) {
+        return DEVICE_DRIVER_GET_OPS(dev, i2c)->transfer(dev, requests, count);
+    }
+    return -ENOSYS;
+}
+
+#endif /* __INCLUDE_NUTTX_DEVICE_I2C_H */
