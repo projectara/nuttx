projectara/nuttx

commit 07bd587f45ba247e9d9ada186d2ed35946da7bf7

svc: switch: Fix support for UNIPRO_MODE_UNCHANGED in link configuration
The UNIPRO_MODE_UNCHANGED constant doesn't fit on 4 bits and thus
overflows to other fields of the pwr_mode variable. Fix it by computing
the pwr_mode value directly instead of preinitializing it and modifying
it. This has the added benefit of simplifying the code.

Testing Done: Tested on DB3 with white camera module
Bug Number: Trivial
Review URL: https://reviews.leaflabs.com/r/2404/
Reviewed by: gregkh, jeanpihet

Signed-off-by: Laurent Pinchart <laurent.pinchart@linaro.org>
Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

nuttx/configs/ara/svc/src/tsb_switch.c
@@ -1946,16 +1946,8 @@ static int switch_prep_for_series_change(struct tsb_switch *sw,
      * PA_HSSeries, but we can set the mode to the same value. Use
      * that to respect the caller's intent to leave a mode
      * unchanged. */
-    if (tx_unchanged || rx_unchanged) {
-        if (tx_unchanged) {
-            *pwr_mode &= ~0x0f;
-            *pwr_mode |= cur_pwr_mode & 0x0f;
-        }
-        if (rx_unchanged) {
-            *pwr_mode &= ~0xf0;
-            *pwr_mode |= cur_pwr_mode & 0xf0;
-        }
-    }
+    *pwr_mode = (tx_unchanged ? cur_pwr_mode & 0x0f : tx->upro_mode)
+              | (rx_unchanged ? cur_pwr_mode & 0xf0 : rx->upro_mode << 4);
 
     /* If both directions of the current mode are ready for the
      * change; there's nothing more to do.
@@ -2318,7 +2310,7 @@ int switch_configure_link(struct tsb_switch *sw,
     uint32_t rx_term = !!(cfg->flags & UPRO_LINKF_RX_TERMINATION);
     uint32_t scrambling = !!(cfg->flags & UPRO_LINKF_SCRAMBLING);
     const struct unipro_pwr_user_data *udata = &cfg->upro_user;
-    uint32_t pwr_mode = tx->upro_mode | (rx->upro_mode << 4);
+    uint32_t pwr_mode;
 
     dbg_verbose("%s(): port=%d\n", __func__, port_id);
 

commit 304649f4f23a1d9b6f5124b1db271614284e48dd

svc: switch: Initialize power mode when keeping HS series unchanged
Commit 07bd587f45ba ("svc: switch: Fix support for
UNIPRO_MODE_UNCHANGED in link configuration") introduced an issue in the
link power mode configuration when no change to the HS series is
requested. The power mode is then left uninitialized, leading to random
results. Fix it by setting the power mode explicitly in that case.

Testing Done: Tested on DB3 with white camera module
Bug Number:
Review URL: https://reviews.leaflabs.com/r//
Reviewed by: eligottlieb, mbolivar, jeanpihet

Signed-off-by: Laurent Pinchart <laurent.pinchart@linaro.org>

nuttx/configs/ara/svc/src/tsb_switch.c
@@ -2315,7 +2315,7 @@ int switch_configure_link(struct tsb_switch *sw,
     uint32_t rx_term = !!(cfg->flags & UPRO_LINKF_RX_TERMINATION);
     uint32_t scrambling = !!(cfg->flags & UPRO_LINKF_SCRAMBLING);
     const struct unipro_pwr_user_data *udata = &cfg->upro_user;
-    uint32_t pwr_mode;
+    uint32_t pwr_mode = tx->upro_mode | (rx->upro_mode << 4);
 
     dbg_verbose("%s(): port=%d\n", __func__, port_id);
 