684632048c1650b1ab9ad0973cec1ad55f149008
Jean Pihet <jean.pihet@newoldbits.com>
2016-01-14 22:30:51 +0100

    Ara: Interface: Remove obsolete interface types
    Testing Done: Build
    Bug Number: SW-1804
    Review URL: https://reviews.leaflabs.com/r/2832/
    Reviewed by: Marti Bolivar, Fabien Parent
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- nuttx/configs/ara/svc/src/interface.h
+++ nuttx/configs/ara/svc/src/interface.h
@@ -87,11 +87,6 @@ struct wd_data {
 enum ara_iface_type {
     /* Connected to built-in UniPro peer (like a bridge ASIC on DB3 e.g.). */
     ARA_IFACE_TYPE_BUILTIN,
-    /* Connected to an interface block (like on an endo, or an
-     * interface block on a BDB). */
-    ARA_IFACE_TYPE_BLOCK,
-    /* Expansion interface to external connectors (e.g. SMA) */
-    ARA_IFACE_TYPE_EXPANSION,
     /* Module port interface, as on DB3 board */
     ARA_IFACE_TYPE_MODULE_PORT,
 };
@@ -200,95 +195,11 @@ uint32_t interface_pm_get_spin(struct interface *iface);
         .debounce_tv = { 0, 0 },                               \
     }
 
-#define __MAKE_BB_WAKEOUT(n) WAKEOUT_SPRING ## n
-#define MAKE_BB_WAKEOUT(n) __MAKE_BB_WAKEOUT(n)
-#define __MAKE_BB_VREG_DATA(n) bb ## n ## _vreg_data
-#define MAKE_BB_VREG_DATA(n) __MAKE_BB_VREG_DATA(n)
 #define __MAKE_BB_PM(n) bb ## n ## _pm
 #define MAKE_BB_PM(n) __MAKE_BB_PM(n)
-#define __MAKE_BB_INTERFACE(n) bb ## n ## _interface
-#define MAKE_BB_INTERFACE(n) __MAKE_BB_INTERFACE(n)
-
-#define DECLARE_SPRING_INTERFACE(number, gpio, portid,         \
-                                 pm_adc, pm_chan, pm_spin,     \
-                                 wake_in_gpio, wake_in_pol,    \
-                                 detect_in_gpio, detect_in_pol)\
-    static struct vreg_data MAKE_BB_VREG_DATA(number)[] = {    \
-        INIT_ACTIVE_LOW_VREG_DATA(gpio, 0)                     \
-    };                                                         \
-    DECLARE_VREG(spring ## number, MAKE_BB_VREG_DATA(number))  \
-                                                               \
-    static struct pm_data MAKE_BB_PM(number)[] = {             \
-        INIT_SPRING_PM_DATA(pm_adc, pm_chan, pm_spin)          \
-    };                                                         \
-                                                               \
-    static struct interface MAKE_BB_INTERFACE(number) = {      \
-        .name = "spring" #number,                              \
-        .if_type = ARA_IFACE_TYPE_BLOCK,                       \
-        .flags = (wake_in_pol ?                                \
-                    ARA_IFACE_FLAG_WAKE_IN_ACTIVE_HIGH :       \
-                    ARA_IFACE_FLAG_WAKE_IN_ACTIVE_LOW) |       \
-                 (detect_in_pol ?                              \
-                    ARA_IFACE_FLAG_DETECT_IN_ACTIVE_HIGH :     \
-                    ARA_IFACE_FLAG_DETECT_IN_ACTIVE_LOW),      \
-        .vreg = &MAKE_VREG(spring ## number),                  \
-        .switch_portid = portid,                               \
-        .wake_out = MAKE_BB_WAKEOUT(number),                   \
-        .pm = MAKE_BB_PM(number),                              \
-        .wake_in = INIT_WD_DATA(wake_in_gpio),                 \
-        .detect_in = INIT_WD_DATA(detect_in_gpio),             \
-        .ejectable = false,                                    \
-        .release_gpio = 0,                                     \
-    };
 
 #define __MAKE_INTERFACE(n) n ## _interface
 #define MAKE_INTERFACE(n) __MAKE_INTERFACE(n)
-
-#define DECLARE_EXPANSION_INTERFACE(_name, vreg_data, portid,  \
-                          _wake_out, wake_in_gpio, wake_in_pol,\
-                          detect_in_gpio, detect_in_pol)       \
-    DECLARE_VREG(_name, vreg_data)                             \
-    static struct interface MAKE_INTERFACE(_name) = {          \
-        .name = #_name,                                        \
-        .if_type = ARA_IFACE_TYPE_EXPANSION,                   \
-        .flags = (wake_in_pol ?                                \
-                    ARA_IFACE_FLAG_WAKE_IN_ACTIVE_HIGH :       \
-                    ARA_IFACE_FLAG_WAKE_IN_ACTIVE_LOW) |       \
-                 (detect_in_pol ?                              \
-                    ARA_IFACE_FLAG_DETECT_IN_ACTIVE_HIGH :     \
-                    ARA_IFACE_FLAG_DETECT_IN_ACTIVE_LOW),      \
-        .vreg = &MAKE_VREG(_name),                             \
-        .switch_portid = portid,                               \
-        .wake_out = _wake_out,                                 \
-        .pm = NULL,                                            \
-        .wake_in = INIT_WD_DATA(wake_in_gpio),                 \
-        .detect_in = INIT_WD_DATA(detect_in_gpio),             \
-        .ejectable = false,                                    \
-        .release_gpio = 0,                                     \
-    };
-
-#define DECLARE_INTERFACE(_name, vreg_data, portid, _wake_out, \
-                          wake_in_gpio, wake_in_pol,           \
-                          detect_in_gpio, detect_in_pol)       \
-    DECLARE_VREG(_name, vreg_data)                             \
-    static struct interface MAKE_INTERFACE(_name) = {          \
-        .name = #_name,                                        \
-        .if_type = ARA_IFACE_TYPE_BUILTIN,                     \
-        .flags = (wake_in_pol ?                                \
-                    ARA_IFACE_FLAG_WAKE_IN_ACTIVE_HIGH :       \
-                    ARA_IFACE_FLAG_WAKE_IN_ACTIVE_LOW) |       \
-                 (detect_in_pol ?                              \
-                    ARA_IFACE_FLAG_DETECT_IN_ACTIVE_HIGH :     \
-                    ARA_IFACE_FLAG_DETECT_IN_ACTIVE_LOW),      \
-        .vreg = &MAKE_VREG(_name),                             \
-        .switch_portid = portid,                               \
-        .wake_out = _wake_out,                                 \
-        .pm = NULL,                                            \
-        .wake_in = INIT_WD_DATA(wake_in_gpio),                 \
-        .detect_in = INIT_WD_DATA(detect_in_gpio),             \
-        .ejectable = false,                                    \
-        .release_gpio = 0,                                     \
-    };
 
 /*
  * Module port interface, as on DB3 board.
@@ -312,9 +223,7 @@ uint32_t interface_pm_get_spin(struct interface *iface);
                     ARA_IFACE_FLAG_DETECT_IN_ACTIVE_LOW),      \
         .vreg = &MAKE_VREG(_name),                             \
         .switch_portid = portid,                               \
-        .wake_out = 0,                                         \
         .pm = NULL,                                            \
-        .wake_in = INIT_WD_DATA(0),                            \
         .detect_in = INIT_WD_DATA(wake_detect_gpio),           \
         .ejectable = _ejectable,                               \
         .release_gpio = _rg,                                   \
