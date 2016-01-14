75911395b641ed7cc772cb6cafaa419f825fc872
Jean Pihet <jean.pihet@newoldbits.com>
2016-01-14 21:55:25 +0100

    Ara: Remove support for BDB development boards
    These boards are not compliant with the HWID feature, which is used for
    the SVC unified build.
    Remove them.
    
    Testing Done: build
    Bug Number: SW-1804
    Review URL: https://reviews.leaflabs.com/r/2830/
    Reviewed by: Marti Bolivar, Fabien Parent
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- nuttx/configs/ara/svc/src/ara_board.h
+++ nuttx/configs/ara/svc/src/ara_board.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Google Inc.
+ * Copyright (c) 2015-2016 Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -57,14 +57,6 @@ enum hwid {
  * STM32 GPIO starts at STM32_GPIO_CHIP_BASE
  */
 #define STM32_GPIO_CHIP_NR      140
-
-/* BDB2A */
-#define U96_GPIO_CHIP_START     (STM32_GPIO_CHIP_BASE + STM32_GPIO_CHIP_NR)
-#define U96_GPIO_CHIP_NR        16
-#define U90_GPIO_CHIP_START     (U96_GPIO_CHIP_START + U96_GPIO_CHIP_NR)
-#define U90_GPIO_CHIP_NR        16
-#define U135_GPIO_CHIP_START    (U90_GPIO_CHIP_START + U90_GPIO_CHIP_NR)
-#define U135_GPIO_CHIP_NR       24
 
 /* SDB */
 #define U701_GPIO_CHIP_START    (STM32_GPIO_CHIP_BASE + STM32_GPIO_CHIP_NR)
