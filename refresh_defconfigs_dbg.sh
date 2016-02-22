8f566f0d6b2da0388144250fb80961714a2d0d7f
Joel Porquet <porquet_joel@projectara.com>
2016-02-22 11:34:47 -0800

    SW-2899: build: replace former building scripts with instructions for new FDK
    The workflow for NuttX development has changed as developers have moved to the
    new FDK. Replace the usual building scripts with a message referring to the new
    workflow and give a hint of which the new instructions should be used instead.
    
    Change-Id: I626ecb4b1ab084924d3831c62f92af7b2e1e8e36
    Signed-off-by: Joel Porquet <porquet_joel@projectara.com>

--- refresh_defconfigs_dbg.sh
+++ refresh_defconfigs_dbg.sh
@@ -1,164 +1,38 @@
 #!/bin/bash
 # refresh_defconfigs.sh
+#
+# Copyright (c) 2016 Google, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
-# define exit error codes
-ARA_BUILD_CONFIG_ERR_BAD_PARAMS=1
-ARA_BUILD_CONFIG_ERR_NO_NUTTX_TOPDIR=2
-ARA_BUILD_CONFIG_ERR_CONFIG_NOT_FOUND=3
-ARA_BUILD_CONFIG_ERR_CONFIG_COPY_FAILED=4
-
-# Other build configuration.
-ARA_BUILD_PARALLEL=1            # controls make's -j flag
-
-echo "Project Ara firmware defconfig refresh"
-echo "--------------------------"
-printenv
-echo "--------------------------"
-
-ARA_BUILD_PARALLEL="-j 6"
-
-# determine NuttX top level folder absolute path
-TOPDIR="`dirname \"$0\"`"  # relative
-TOPDIR="`( cd \"$TOPDIR/nuttx\" && pwd )`"  # absolutized and normalized
-if [ -z "$TOPDIR" ] ; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions)
-  echo "Can't determine NuttX top level folder, fatal."
-  exit $ARA_BUILD_CONFIG_ERR_NO_NUTTX_TOPDIR
-fi
-
-rebuild_defconfig() {
-  # configpath, defconfigFile, buildname, buildbase
-  # must be defined on entry
-  echo "rebuild_defconfig() start"
-  echo "buildname='$buildname'"
-
-  # define paths used during build process
-  ARA_BUILD_CONFIG_PATH="$buildbase/$buildname/config"
-  ARA_BUILD_TOPDIR="$buildbase/$buildname"
-
-  # delete build tree if it already exists
-  if [ -d $ARA_BUILD_TOPDIR ] ; then
-    rm -rf $ARA_BUILD_TOPDIR
-  fi
-
-  # create folder structure in build output tree
-  mkdir -p "$ARA_BUILD_CONFIG_PATH"
-  mkdir -p "$ARA_BUILD_TOPDIR"
-
-  # Copy nuttx tree to build tree
-  cp -r $TOPDIR/../nuttx $ARA_BUILD_TOPDIR/nuttx
-  cp -r $TOPDIR/../apps $ARA_BUILD_TOPDIR/apps
-  cp -r $TOPDIR/../misc $ARA_BUILD_TOPDIR/misc
-
-  # copy Make.defs to build output tree
-  if ! install -m 644 -p ${configpath}/Make.defs ${ARA_BUILD_TOPDIR}/nuttx/Make.defs  >/dev/null 2>&1; then
-      echo "Warning: Failed to copy Make.defs"
-  fi
-
-  # copy setenv.sh to build output tree
-  if  install -p ${configpath}/setenv.sh ${ARA_BUILD_TOPDIR}/nuttx/setenv.sh >/dev/null 2>&1; then
-  chmod 755 "${ARA_BUILD_TOPDIR}/nuttx/setenv.sh"
-  fi
-
-  echo "Copying defconfig for '$buildname' to build output tree"
-  # copy defconfig to build output tree
-  if ! install -m 644 -p ${defconfigFile} ${ARA_BUILD_TOPDIR}/nuttx/.config ; then
-      echo "ERROR: Failed to copy defconfig"
-      exit $ARA_BUILD_CONFIG_ERR_CONFIG_COPY_FAILED
-  fi
-
-  echo "Saving config files for '$buildname'"
-  # save config files
-  cp ${ARA_BUILD_TOPDIR}/nuttx/.config   ${ARA_BUILD_CONFIG_PATH}/.config > /dev/null 2>&1
-  cp ${ARA_BUILD_TOPDIR}/nuttx/Make.defs ${ARA_BUILD_CONFIG_PATH}/Make.defs > /dev/null 2>&1
-  cp ${ARA_BUILD_TOPDIR}/nuttx/setenv.sh  ${ARA_BUILD_CONFIG_PATH}/setenv.sh > /dev/null 2>&1
-
-  echo "Building config for '$buildname'"
-  export ARA_BUILD_NAME=$buildname
-  pushd $ARA_BUILD_TOPDIR/nuttx > /dev/null
-
-#  make -j ${ARA_BUILD_PARALLEL} -r -f Makefile.unix  olddefconfig > /dev/null 2>&1
-  make -j ${ARA_BUILD_PARALLEL} -d -r -f Makefile.unix  olddefconfig
-
-  MAKE_RESULT=${PIPESTATUS[0]}
-
-  popd > /dev/null
-  echo "rebuild_defconfig() end"
-}
-
-copy_defconfig_file() {
-    echo "copy_defconfig_file()"
-    # uses configpath
-    if  ! diff -q $ARA_BUILD_TOPDIR/nuttx/.config $configpath/defconfig > /dev/null  ; then
-        echo "Updating defconfig"
-        cp $ARA_BUILD_TOPDIR/nuttx/.config $configpath/defconfig
-    fi
-}
-
-remove_build() {
-  echo "remove_build()"
-  # uses buildbase and buildname
-  rm -rf "$buildbase/$buildname"
-}
-
-canonicalize() {
-    TARGET_FILE=$1
-
-    cd `dirname $TARGET_FILE`
-    TARGET_FILE=`basename $TARGET_FILE`
-
-    # Iterate down a (possible) chain of symlinks
-    while [ -L "$TARGET_FILE" ]
-    do
-        TARGET_FILE=`readlink $TARGET_FILE`
-        cd `dirname $TARGET_FILE`
-        TARGET_FILE=`basename $TARGET_FILE`
-    done
-
-    # Compute the canonicalized name by finding the physical path
-    # for the directory we're in and appending the target file.
-    PHYS_DIR=`pwd -P`
-    RESULT=$PHYS_DIR/$TARGET_FILE
-    echo $RESULT
-}
-
-# set build output path
-buildbase="`( cd \"$TOPDIR/..\" && pwd )`/build"
-echo "buildbase='$buildbase'"
-# build list of defconfigs
-defconfig_list=$(find $TOPDIR/configs/ara  -iname defconfig)
-
-# build the kconfig tools if we haven't already.  Only need to do this once.
-echo "prebuilding kconfig tools"
-pushd "${TOPDIR}" > /dev/null
-make -f Makefile.unix buildconfig
-popd > /dev/null
-
-# process list of defconfigs
-for cfg in $defconfig_list; do
-  echo "Checking '$cfg'"
-  # save full path to defconfig
-  defconfigFile=${cfg}
-  # get abs path to defconfig
-  configpath=$(canonicalize  $(dirname "$cfg"))
-  #create build name
-  buildname=$(canonicalize $(dirname "$cfg"))
-  #strip abs path
-  buildname=$(echo "$buildname" | sed -e "s:^$TOPDIR/configs/::")
-  # repl slash with dash
-  buildname=$(echo "$buildname" | sed -e "s#/#-#g")
-  # rebuild the defconfig
-  rebuild_defconfig
-  # check build result
-  if [ $MAKE_RESULT -ne 0 ] ; then
-    echo "Rebuild defconfig for '$buildname' failed"
-    exit 1
-  fi
-  copy_defconfig_file
-  remove_build
-done
-echo "Refresh defconfigs completed"
-exit 0
-
+echo "*************************************************************************"
+echo "Warning: The workflow for NuttX has changed. Please use the new FDK."
+echo ""
+echo "This script is typically replaced by:"
+echo "$ build-fdk.sh -v updateconfig [path/to/mkfile|all|all-frame]"
+echo "*************************************************************************"
+exit 1
 
