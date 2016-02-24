21bff1b98a35a252e4b18f5d2f9d12e2b29e54ba
Jean Pihet <jean.pihet@newoldbits.com>
2016-02-24 11:41:36 +0100

    SW-2085: SVC: Session data read: return actual data length
    The Switch can signal different data lengths in the status
    report and in the actual session data header. This is the case
    when data reception is busy or when multiple incoming messages
    have been received.
    Retrieve the actual session data length from the header and return it
    for parsing.
    
    Add verbose debug messages for the status report and the session data
    content.
    
    Testing Done: DB3.5: hotplug/unplug speaker module, check connections
    and SPI messages content.
    
    Change-Id: Ib3329e8271b3c09690912cc4e97d73b2e6176784
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- nuttx/configs/ara/svc/src/tsb_switch_es3.c
+++ nuttx/configs/ara/svc/src/tsb_switch_es3.c
@@ -374,6 +374,9 @@ static int es3_read_status(struct tsb_switch *sw,
 
     _switch_spi_select(sw, false);
 
+    dbg_insane("SRPT RX:\n");
+    dbg_print_buf(ARADBG_INSANE, rxbuf, SRPT_SIZE);
+
     /* Find the header */
     for (offset = 0; offset < (SRPT_SIZE - SRPT_REPORT_SIZE); offset++) {
         if (!memcmp(srpt_report_header,
@@ -410,6 +413,9 @@ static int es3_read_status(struct tsb_switch *sw,
         status->tx_fifo_size = rpt->raw[7] * 8;
         status->rx_fifo_size = fifo_max_size -
                                ((rpt->raw[10] << 8) | rpt->raw[11]);
+
+        dbg_insane("SRPT: TX%u, RX%u\n", status->tx_fifo_size,
+                   status->rx_fifo_size);
     }
 
     return 0;
@@ -490,7 +496,7 @@ out:
 static int es3_session_read(struct tsb_switch *sw,
                             uint8_t cportid,
                             uint8_t *rx_buf,
-                            size_t rx_size) {
+                            size_t *rx_size) {
     struct sw_es3_priv *priv = sw->priv;
     struct spi_dev_s *spi_dev = sw->spi_dev;
     uint8_t *rxbuf = fifo_to_rxbuf(priv, cportid);
@@ -512,7 +518,7 @@ static int es3_session_read(struct tsb_switch *sw,
     // Read CNF and retry if NACK received
     do {
         // Read the CNF
-        size = ES3_SWITCH_WAIT_REPLY_LEN + rx_size + SWITCH_DATA_READ_LEN;
+        size = ES3_SWITCH_WAIT_REPLY_LEN + *rx_size + SWITCH_DATA_READ_LEN;
         SPI_SNDBLOCK(spi_dev, read_header, sizeof read_header);
         SPI_EXCHANGE(spi_dev, NULL, rxbuf, size);
         /* Make sure we use 16-bit frames */
@@ -520,7 +526,7 @@ static int es3_session_read(struct tsb_switch *sw,
             SPI_SEND(spi_dev, LNUL);
         }
 
-        dbg_insane("RX Data:\n");
+        dbg_insane("RX raw Data (%u):\n", size);
         dbg_print_buf(ARADBG_INSANE, rxbuf, size);
 
         if (!rx_buf) {
@@ -550,6 +556,8 @@ static int es3_session_read(struct tsb_switch *sw,
                 resp_start = &rxbuf[i];
                 size_t resp_len = resp_start[2] << 8 | resp_start[3];
                 memcpy(rx_buf, &resp_start[4], resp_len);
+                /* Return the actual message len */
+                *rx_size = resp_len;
                 rcv_done = 1;
                 break;
             } else if (rxbuf[i] == NACK) {
@@ -596,17 +604,20 @@ static int es3_irq_fifo_rx(struct tsb_switch *sw, unsigned int cportid) {
         rc = -EIO;
         goto fill_done;
     }
+    /* Get the raw RX len */
     len = rpt.rx_fifo_size;
 
     /*
      * Drain the fifo data.
+     * Pass the raw RX len and get the actual session data length back.
      */
-    rc = es3_session_read(sw, cportid, cport->rxbuf, len);
+    rc = es3_session_read(sw, cportid, cport->rxbuf, &len);
     if (rc) {
         dbg_error("%s: read failed: %d\n", __func__, rc);
         rc = -EIO;
         goto fill_done;
     }
+    dbg_verbose("RX Data (%u):\n", len);
     dbg_print_buf(ARADBG_VERBOSE, cport->rxbuf, len);
 
 fill_done:
