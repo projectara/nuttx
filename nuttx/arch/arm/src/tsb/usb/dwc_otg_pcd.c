f88feecc608d59a8b0c7604bb9a3de32da3c1375
Alexandre Bailon <abailon@baylibre.com>
2016-03-08 11:40:20 +0100

    SW-3518: DWC OTG: Fix bulk in request cancelation
    Currently, we are testing if the request is present
    in ring before to cancel it. That only make sens for
    bulk out endpoint so only do that test for bulk out.
    
    Testing Done: Watchdog can cancel request and unblock USB
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    
    Change-Id: I52976aa9cd8dd748ef3245ab2ab72af9d3cffff0

--- nuttx/arch/arm/src/tsb/usb/dwc_otg_pcd.c
+++ nuttx/arch/arm/src/tsb/usb/dwc_otg_pcd.c
@@ -3012,7 +3012,8 @@ int dwc_otg_pcd_ep_dequeue(dwc_otg_pcd_t * pcd, void *ep_handle,
 
 #ifdef DWC_ENHANCED_SG_DMA_OUT
 	/* Invalidate the entry in the ring to cause a BNA */
-	if (invalidate_ring_entry(&ep->dwc_ep, req->dma_desc) < 0) {
+	if (!ep->dwc_ep.is_in &&
+		invalidate_ring_entry(&ep->dwc_ep, req->dma_desc) < 0) {
 		DWC_SPINUNLOCK_IRQRESTORE(pcd->lock, flags);
 		return -DWC_E_INVALID;
 	}
