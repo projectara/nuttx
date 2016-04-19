034f1a280f6d13daeae4c222d5e706c009957653
Fabien Parent <fparent@baylibre.com>
2016-04-19 15:55:50 +0800

    SW-4293: greybus: sdio: use api that don't sleep to send events notification
    We cannot sleep in IRQ context, use the _send_request_nowait() API to avoid
    sleeping.
    
    Testing Done: compiled.
    
    Change-Id: I22cb9eca55e36e3ac4061bd3a19f8fb70133e7d4
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- nuttx/drivers/greybus/sdio.c
+++ nuttx/drivers/greybus/sdio.c
@@ -102,7 +102,7 @@ static int event_callback(void *data, uint8_t event)
     request = gb_operation_get_request_payload(operation);
     request->event = event;
 
-    gb_operation_send_request(operation, NULL, false);
+    gb_operation_send_request_nowait(operation, NULL, false);
     gb_operation_destroy(operation);
 
     return 0;
