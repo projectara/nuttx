c6f0f37c1bd60d301a7131e90073b9ac2ca0975a
Fabien Parent <fparent@baylibre.com>
2016-04-19 15:56:49 +0800

    SW-4293: greybus: power_supply: use api that don't sleep to send events notif
    We cannot sleep in IRQ context, use the _send_request_nowait() API to avoid
    sleeping.
    
    Testing Done: compiled.
    
    Change-Id: I2f47bf5f9c2082e090e1370ce3154e1a8081a67f
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- nuttx/drivers/greybus/power_supply.c
+++ nuttx/drivers/greybus/power_supply.c
@@ -79,7 +79,7 @@ static int event_callback(void *data, uint8_t psy_id, uint8_t event)
     request->psy_id = psy_id;
     request->event = event;
 
-    gb_operation_send_request(operation, NULL, false);
+    gb_operation_send_request_nowait(operation, NULL, false);
     gb_operation_destroy(operation);
 
     return 0;
